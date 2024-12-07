b17d280bcd32ceb92d79c60fa4b5b3b985ed18a3
Anuska86 <anasrm86@gmail.com>
2024-12-07 15:30:46 +0100

    Navbar

--- react-project/src/components/Navbar.jsx
+++ react-project/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 export default function Navbar() {
-    return (
-        <nav>
-            <h1>Navbar goes here</h1>
-        </nav>
-    )
+  return (
+    <header>
+      <nav>
+      <img className="react-facts" src="src/imgs/react-facts.png" alt="React Facts" />
+        <h1>Navbar goes here</h1>
+
+      </nav>
+    </header>
+  );
 }
