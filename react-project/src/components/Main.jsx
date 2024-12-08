89e2a467929793de05128f5aecc7a16e7629acf0
Anuska86 <anasrm86@gmail.com>
2024-12-08 15:40:06 +0100

    main

--- react-project/src/components/Main.jsx
+++ react-project/src/components/Main.jsx
@@ -2,6 +2,21 @@ export default function App() {
   return (
     <main>
       <h1>Main goes here</h1>
+      <ul>
+        <li>
+          React was initially developed by Jordan Walke, a software engineer at
+          Facebook, and it was first deployed on Facebook’s newsfeed in 2011.
+        </li>
+        <li>It was later open-sourced in 2013.</li>
+        <li>
+          React’s primary goal was to solve the problem of efficiently updating
+          the user interface in large-scale applications.
+        </li>
+        <li>
+          In the React universe, there lies a mysterious concept known as the
+          React Fiber.
+        </li>
+      </ul>
     </main>
   );
 }
--- react-project/src/components/Navbar.jsx
+++ react-project/src/components/Navbar.jsx
@@ -3,27 +3,11 @@ export default function Navbar() {
     <header>
       <nav>
         <img
-          className="react-facts"
           src="src/imgs/react-facts.png"
-          alt="React Facts"
+          alt="React facts"
         />
-        <h1>Navbar goes here</h1>
-        <ul>
-          <li>
-            React was initially developed by Jordan Walke, a software engineer
-            at Facebook, and it was first deployed on Facebook’s newsfeed in
-            2011.{" "}
-          </li>
-          <li>It was later open-sourced in 2013.</li>
-          <li>
-            React’s primary goal was to solve the problem of efficiently
-            updating the user interface in large-scale applications.
-          </li>
-          <li>
-            In the React universe, there lies a mysterious concept known as the
-            React Fiber.
-          </li>
-        </ul>
+        <span><h1>Navbar goes here</h1></span>
+        
       </nav>
     </header>
   );
