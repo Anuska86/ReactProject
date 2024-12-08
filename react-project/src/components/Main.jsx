3b938562019a0df19cf0c85e5083a083f5042f1d
Anuska86 <anasrm86@gmail.com>
2024-12-08 17:34:27 +0100

    style

--- react-project/src/components/Main.jsx
+++ react-project/src/components/Main.jsx
@@ -2,21 +2,30 @@ export default function App() {
   return (
     <main>
       <h1>Learning fun facts about React: </h1>
-      <ul className="react-facts">
-        <li>
-          React was initially developed by Jordan Walke, a software engineer at
-          Facebook, and it was first deployed on Facebook’s newsfeed in 2011.
-        </li>
-        <li>It was later open-sourced in 2013.</li>
-        <li>
-          React’s primary goal was to solve the problem of efficiently updating
-          the user interface in large-scale applications.
-        </li>
-        <li>
-          In the React universe, there lies a mysterious concept known as the
-          React Fiber.
-        </li>
-      </ul>
+      <div>
+        <div>
+          <img
+            src="src/imgs/react-logo.png"
+            alt="React logo"
+          />
+        </div>
+        <ul className="react-facts">
+          <li>
+            React was initially developed by Jordan Walke, a software engineer
+            at Facebook, and it was first deployed on Facebook’s newsfeed in
+            2011.
+          </li>
+          <li>It was later open-sourced in 2013.</li>
+          <li>
+            React’s primary goal was to solve the problem of efficiently
+            updating the user interface in large-scale applications.
+          </li>
+          <li>
+            In the React universe, there lies a mysterious concept known as the
+            React Fiber.
+          </li>
+        </ul>
+      </div>
     </main>
   );
 }
